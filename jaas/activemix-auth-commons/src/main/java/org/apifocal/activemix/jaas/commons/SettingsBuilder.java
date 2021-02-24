43e170e2f1f0b1d91fd51c7ffb0efb15aa6878c0
Hadrian Zbarcea <hadrian@apache.org>
2021-02-24 16:02:22 -0500

    Refactor checkpoint. Move reusable functionality to SettingsBuilder

---
+++ jaas/activemix-auth-commons/src/main/java/org/apifocal/activemix/jaas/commons/SettingsBuilder.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2017-2021 apifocal LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apifocal.activemix.jaas.commons;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Settings utility for reading LoginModule configuration properties in more handy way.
+ */
+public final class SettingsBuilder {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SettingsBuilder.class);
+
+
+    public static <T> List<T> createObjects(
+    		final ClassLoader cl, final Settings settings, 
+    		String packageName, String typeNames, Class<T> baseType, String configPrefix) {
+        return Arrays.stream(typeNames.split(","))
+            .map(String::trim)
+            .map(clazz -> load(packageName, clazz, baseType, cl))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .map(validatorClass -> create(validatorClass, settings.subset(configPrefix, validatorClass.getSimpleName())))
+            .collect(Collectors.toList());
+    }
+
+    private static <T> T create(Class<T> typeClass, Settings settings) {
+        try {
+            Constructor<T> constructor = typeClass.getConstructor(Settings.class);
+            return constructor.newInstance(settings);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException("Could not find a constructor with Settings argument in type " + typeClass.getName(), e);
+        } catch (InstantiationException e) {
+            throw new IllegalStateException("Could not instantiate type " + typeClass.getName(), e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException("Could not access a constructor with Settings argument in type " + typeClass.getName(), e);
+        } catch (InvocationTargetException e) {
+            throw new IllegalStateException("Could not initialize instance of type " + typeClass.getName(), e.getTargetException());
+        }
+    }
+
+    public static <T> Optional<Class<T>> load(String pkg, String clazz, Class<T> baseType, ClassLoader classLoader) {
+        if (clazz.contains(".")) {
+            return loadClass(classLoader, baseType, clazz);
+        }
+
+        return Optional.of(pkg)
+            .map(String::trim)
+            .filter(prefix -> !prefix.isEmpty() && !clazz.contains("."))
+            .map(prefix -> loadClass(classLoader, baseType, prefix + "." + clazz))
+            .orElseGet(() -> loadClass(classLoader, baseType, clazz));
+    }
+
+    public static <T> Optional<Class<T>> loadClass(ClassLoader classLoader, Class<T> baseType, String clazz) {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(new CompositeClassLoader(contextClassLoader, classLoader));
+            Class<?> aClass = classLoader.loadClass(clazz);
+            if (baseType.isAssignableFrom(aClass)) {
+                return Optional.of(aClass.asSubclass(baseType));
+            }
+        } catch (ClassNotFoundException e) {
+            LOG.info("Failed to find type {}", clazz);
+        } finally {
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        }
+        return Optional.empty();
+    }
+
+    public static Supplier<? extends RuntimeException> requiredPropertyMissing(String optionName) {
+        return () -> new IllegalStateException("Option " + optionName + " must be specified");
+    }
+
+    public static class CompositeClassLoader extends ClassLoader {
+        private final Logger logger = LoggerFactory.getLogger(CompositeClassLoader.class);
+        private final ClassLoader[] classLoaders;
+
+        CompositeClassLoader(ClassLoader ... classLoaders) {
+            this.classLoaders = classLoaders;
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            for (ClassLoader loader : classLoaders) {
+                try {
+                    return loader.loadClass(name);
+                } catch (ClassNotFoundException e) {
+                    logger.debug("Couldn't find class {} in class laoder {}", name, loader);
+                }
+            }
+            return super.loadClass(name);
+        }
+    }
+
+}
--- jaas/activemix-auth-token/src/main/java/org/apifocal/activemix/jaas/token/TokenLoginModule.java
+++ jaas/activemix-auth-token/src/main/java/org/apifocal/activemix/jaas/token/TokenLoginModule.java
@@ -22,6 +22,7 @@ import com.nimbusds.jwt.SignedJWT;
 
 import org.apifocal.activemix.jaas.commons.ClaimMapper;
 import org.apifocal.activemix.jaas.commons.Settings;
+import org.apifocal.activemix.jaas.commons.SettingsBuilder;
 import org.apifocal.activemix.jaas.commons.TokenValidationException;
 import org.apifocal.activemix.jaas.commons.TokenValidator;
 import org.apifocal.activemix.jaas.commons.verifiers.TokenSecurityContext;
@@ -69,7 +70,7 @@ public class TokenLoginModule implements LoginModule {
     public static final String CLAIM_MAPPERS_PACKAGE = CLAIM_MAPPERS_PREFIX + ".package";
     public static final String CLAIM_MAPPERS_CLASSES = CLAIM_MAPPERS_PREFIX + ".classes";
 
-    private final Logger logger = LoggerFactory.getLogger(TokenLoginModule.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TokenLoginModule.class);
 
     private CallbackHandler callbackHandler;
     private Subject subject;
@@ -97,14 +98,14 @@ public class TokenLoginModule implements LoginModule {
         String verifiersPackage = settings.stringOption(VERIFIERS_PACKAGE, "org.apifocal.amx.jaas.token.verifiers");
         Optional<String> verifiersClasses = settings.stringOption(VERIFIERS_CLASSES);
 
-        String verifierNames = verifiersClasses.orElseThrow(requiredPropertyMissing(VERIFIERS_CLASSES));
-        this.validators = createObjects(verifiersPackage, verifierNames, TokenValidator.class, VERIFIERS_PREFIX);
+        String verifierNames = verifiersClasses.orElseThrow(SettingsBuilder.requiredPropertyMissing(VERIFIERS_CLASSES));
+        this.validators = SettingsBuilder.createObjects(getClass().getClassLoader(), settings, verifiersPackage, verifierNames, TokenValidator.class, VERIFIERS_PREFIX);
 
         String mappersPackage = settings.stringOption(CLAIM_MAPPERS_PACKAGE, "org.apifocal.amx.jaas.token.mappers");
         Optional<String> mappersClasses = settings.stringOption(CLAIM_MAPPERS_CLASSES);
 
-        String mapperNames = mappersClasses.orElseThrow(requiredPropertyMissing(CLAIM_MAPPERS_CLASSES));
-        this.claimMappers = createObjects(mappersPackage, mapperNames, ClaimMapper.class, CLAIM_MAPPERS_PREFIX);
+        String mapperNames = mappersClasses.orElseThrow(SettingsBuilder.requiredPropertyMissing(CLAIM_MAPPERS_CLASSES));
+        this.claimMappers = SettingsBuilder.createObjects(getClass().getClassLoader(), settings, mappersPackage, mapperNames, ClaimMapper.class, CLAIM_MAPPERS_PREFIX);
 
         if (validators.isEmpty() || claimMappers.isEmpty()) {
             throw new IllegalStateException("Both validators and claim mappers must be set");
@@ -133,7 +134,7 @@ public class TokenLoginModule implements LoginModule {
             return (this.claims = processToken(new String(token))) != null;
         } catch (ParseException e) {
             if (verbose) {
-                logger.warn("Failed to process token", e);
+                LOG.warn("Failed to process token", e);
             }
             throw new LoginException("Invalid token");
         }
@@ -152,7 +153,7 @@ public class TokenLoginModule implements LoginModule {
                 validator.validate(parsedToken, securityContext);
             } catch (TokenValidationException e) {
                 if (verbose) {
-                    logger.warn("Detected invalid token", e);
+                    LOG.warn("Detected invalid token", e);
                 }
                 throw new LoginException("Token didn't pass " + validator.getClass().getName() + " validation");
             }
@@ -189,87 +190,4 @@ public class TokenLoginModule implements LoginModule {
         return true;
     }
 
-    // additional methods required to handle configuration
-
-    private <T> List<T> createObjects(String packageName, String typeNames, Class<T> baseType, String configPrefix) {
-        return Arrays.stream(typeNames.split(","))
-            .map(String::trim)
-            .map(clazz -> load(packageName, clazz, baseType, getClass().getClassLoader()))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .map(validatorClass -> create(validatorClass, settings.subset(configPrefix, validatorClass.getSimpleName())))
-            .collect(Collectors.toList());
-    }
-
-    private static <T> T create(Class<T> typeClass, Settings settings) {
-        try {
-            Constructor<T> constructor = typeClass.getConstructor(Settings.class);
-            return constructor.newInstance(settings);
-        } catch (NoSuchMethodException e) {
-            throw new IllegalStateException("Could not find a constructor with Settings argument in type " + typeClass.getName(), e);
-        } catch (InstantiationException e) {
-            throw new IllegalStateException("Could not instantiate type " + typeClass.getName(), e);
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException("Could not access a constructor with Settings argument in type " + typeClass.getName(), e);
-        } catch (InvocationTargetException e) {
-            throw new IllegalStateException("Could not initialize instance of type " + typeClass.getName(), e.getTargetException());
-        }
-    }
-
-    protected <T> Optional<Class<T>> load(String pkg, String clazz, Class<T> baseType, ClassLoader classLoader) {
-        if (clazz.contains(".")) {
-            return loadClass(classLoader, baseType, clazz);
-        }
-
-        return Optional.of(pkg)
-            .map(String::trim)
-            .filter(prefix -> !prefix.isEmpty() && !clazz.contains("."))
-            .map(prefix -> loadClass(classLoader, baseType, prefix + "." + clazz))
-            .orElseGet(() -> loadClass(classLoader, baseType, clazz));
-    }
-
-    protected <T> Optional<Class<T>> loadClass(ClassLoader classLoader, Class<T> baseType, String clazz) {
-        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-        try {
-            Thread.currentThread().setContextClassLoader(new CompositeClassLoader(contextClassLoader, classLoader));
-            Class<?> aClass = classLoader.loadClass(clazz);
-            if (baseType.isAssignableFrom(aClass)) {
-                return Optional.of(aClass.asSubclass(baseType));
-            }
-        } catch (ClassNotFoundException e) {
-            if (verbose) {
-                logger.warn("Could not find type {}", clazz, e);
-            } else {
-                logger.info("Failed to find type {}", clazz);
-            }
-        } finally {
-            Thread.currentThread().setContextClassLoader(contextClassLoader);
-        }
-        return Optional.empty();
-    }
-
-    private static Supplier<? extends RuntimeException> requiredPropertyMissing(String optionName) {
-        return () -> new IllegalStateException("Option " + optionName + " must be specified");
-    }
-
-    private static class CompositeClassLoader extends ClassLoader {
-        private final Logger logger = LoggerFactory.getLogger(CompositeClassLoader.class);
-        private final ClassLoader[] classLoaders;
-
-        CompositeClassLoader(ClassLoader ... classLoaders) {
-            this.classLoaders = classLoaders;
-        }
-
-        @Override
-        public Class<?> loadClass(String name) throws ClassNotFoundException {
-            for (ClassLoader loader : classLoaders) {
-                try {
-                    return loader.loadClass(name);
-                } catch (ClassNotFoundException e) {
-                    logger.debug("Couldn't find class {} in class laoder {}", name, loader);
-                }
-            }
-            return super.loadClass(name);
-        }
-    }
 }
